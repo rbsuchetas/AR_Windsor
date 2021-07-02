e82d543ca83213ca5b42db200b4a1514078be3bd
SaiPrasannaPolagani <85219517+SaiPrasannaPolagani@users.noreply.github.com>
2021-07-02 13:28:06 +0530

    UI and CSS styling of menu page - SaiPrasannaPolagani
    UI and CSS styling of menu page - SaiPrasannaPolagani

--- src/Components/MenuTest.js
+++ src/Components/MenuTest.js
@@ -1,11 +1,24 @@
 import { ListItem } from "@material-ui/core";
 import React, { Component, useState, useEffect } from "react";
-import { Platform, StyleSheet, Text, View, ScrollView, SectionList ,FlatList, TouchableOpacity, Image,
-ActivityIndicator, Linking } from "react-native";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  SectionList,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  ActivityIndicator,
+  Linking,
+} from "react-native";
+import { Tile } from "react-native-elements";
 import Separator from "./Separator";
 // import { Card } from "react-native-elements/dist/card/Card";
-import { WebView } from 'react-native-webview';
+import { WebView } from "react-native-webview";
 import { render } from "react-dom";
+import { RepeatRounded } from "@material-ui/icons";
 
 export default function MenuTest({ route, navigation }) {
   const { clicked, restParam, foodParam, restParamId } = route.params;
@@ -14,90 +27,111 @@ export default function MenuTest({ route, navigation }) {
   const [foData, setFoData] = useState(foodParam);
   const [resId, setResId] = useState(restParamId);
 
-
-  navigation.setOptions({ title: nameOfPage })
-
+  navigation.setOptions({ title: nameOfPage });
+  // console.log(foodParam);
   useEffect(() => {
-      console.log(resId)
-     }, []);
+    console.log(resId);
+  }, []);
 
-
- return (   
-        <View style={styles.container}>
-            {(() => {
-             if (resId % 1 === 0){
-                  return (
-                      <FlatList
-                    data={foData}
-                    renderItem={({item}) => (
-                    <View>
-                     {(() => {
-                         if (item.fid == resId){
-                         return (
-                             <TouchableOpacity onPress={() => navigation.navigate('ArModels', { clicked: item.fname })}>
-                                    <View>  
-                                        <Text> {item.fname} </Text>
-                                        <Text> ${item.fprice} </Text>
-                                         <Image source={{ uri: item.fimg }} style={styles.ImageIconStyle} />
-                                         
-                                      </View>
-                                      <View
-  style={{
-    borderBottomColor: 'black',
-    borderBottomWidth: 1,
-  }}
-/>
-                                  </TouchableOpacity>    
-                                      
-                            )
-                    }
-
-                    })()}
-                    
-                     
-                         
-                    
-                    </View>
-                ) }
-               />
-                  )
-              }
-
-
-              else{
-                  return(
-                      <View>
-                          <Text> Menu to be updated. Please stay tuned to this page. </Text>
+  return (
+    <View style={styles.container}>
+      {(() => {
+        if (resId % 1 === 0) {
+          return (
+            <FlatList
+              data={foData}
+              renderItem={({ item }) => (
+                <View>
+                  {(() => {
+                    if (item.fid == resId) {
+                      return (
+                        <TouchableOpacity
+                          onPress={() =>
+                            navigation.navigate("ArModels", {
+                              clicked: item.fname,
+                            })
+                          }
+                        >
+                          <View>
+                            <Image
+                              source={{ uri: item.fimg }}
+                              style={styles.ImageIconStyle}
+                            />
+                            <Text style={styles.TextStyle}> {item.fname} </Text>
+                            <Text style={styles.TextStyle}>
+                              {" "}
+                              ${item.fprice}{" "}
+                            </Text>
                           </View>
-                  )
-              }
-              
-          
-            })()}
-        </View>
-    )
-
-    
-
+                          <View
+                            style={{
+                              borderBottomColor: "black",
+                              borderBottomWidth: 1,
+                            }}
+                          />
+                          <View style={styles.SeparatorLine} />
+                        </TouchableOpacity>
+                      );
+                    }
+                  })()}
+                </View>
+              )}
+            />
+          );
+        } else {
+          return (
+            <View>
+              <Text> Menu to be updated. Please stay tuned to this page. </Text>
+            </View>
+          );
+        }
+      })()}
+    </View>
+  );
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
-   flex: 1,
-   paddingTop: 22
+    flex: 1,
+    // paddingTop: 22,
+    backgroundColor: "#F5FCFF",
   },
   item: {
-    padding: 10,
+    padding: 50,
     fontSize: 18,
     height: 44,
   },
-   ImageIconStyle: {
-   padding: 20,
-   margin: 0,
-   height: 80,
-   width: 80,
-   resizeMode : 'stretch',
-   }
+  SeparatorLine: {
+    width: 10,
+    marginTop: 30,
+  },
+  ImageIconStyle: {
+    padding: 20,
+    margin: 0,
+    height: 170,
+    width: 350,
+    resizeMode: "stretch",
+    // borderTopRightRadius: 20,
+    // borderBottomRightRadius: 20,
+    // borderBottomLeftRadius: 20,
+    // borderTopLeftRadius: 20,
+
+    //marginTop: 50,
+    // resizeMode: "cover",
+    flexDirection: "row",
+  },
+  TextStyle: {
+    //color: "#949494",
+    // fontSize: 16,
+    fontWeight: "600",
+    fontSize: 18,
+    color: "#000",
+    marginTop: 2,
+    //color: "black",
+    // marginBottom: 10,
+    // marginRight: 0,
+    // alignSelf: "flex-end",
+    // fontSize: 20,
+    // paddingRight: 30,
+  },
 });
