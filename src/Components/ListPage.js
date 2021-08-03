93e1198cd476665d8a93f74ad761ac021feff338
SaiPrasannaPolagani <85219517+SaiPrasannaPolagani@users.noreply.github.com>
2021-08-03 21:45:18 +0530

    Updated Required Changes- SaiPrasannaPolagani
    Updated Required Changes- SaiPrasannaPolagani

--- src/Components/ListPage.js
+++ src/Components/ListPage.js
@@ -1,5 +1,14 @@
-import React, { useState} from "react";
-import { StyleSheet, Text, View,FlatList, TouchableOpacity, Image, SafeAreaView, TextInput} from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+  TextInput,
+} from "react-native";
 import Separator from "./Separator";
 
 export default function ListPage({ route, navigation }) {
@@ -36,76 +45,90 @@ export default function ListPage({ route, navigation }) {
   };
 
   return (
-      <View style={styles.container}>
-        <SafeAreaView style={{ flex: 1 }}>
-          {!tourData && !eventsData &&
-        <TextInput
-          style={styles.textInputStyle}
-          value={search}
-          underlineColorAndroid="transparent"
-          placeholder="Search Here"
-          onChangeText={(text) => searchFilter(text)}
-        />
-          }
-          <FlatList
-            data={tourismData ? tourData :eventData ? eventsData: filData   }
-            keyExtractor={item => tourData? item.tname : item.name}
-            renderItem={({ item }) => (
-              <TouchableOpacity onPress={
-                () => {
-                  !tourismData && !eventData ?  
-                  navigation.navigate('menu', 
-                    { 
-                      title: item.name, 
-                      restParam: resData,
-                      foodParam: fooData, 
-                      restParamId: item.uid 
-                    })
-                    : tourismData ? 
-                    navigation.navigate('TourismDestination', 
-                    { 
-                      title: item.tname,
-                      desc: item.tdesc, 
-                      img: item.timg, 
-                      model: item.tmodel, 
-                      url: item.turl 
-                    })
-                   
-                  :
-                  navigation.navigate('TourismDestination', 
-                    { 
-                      title: item.billName,
-                      desc: item.billDesc, 
-                      img: item.billPic,  
-                      url: item.billUrl 
-                    })
-                  } 
-                  }
-            >
-            <View style={styles.row}>
-              <View style={styles.imageHeader}>
-                <Image 
-                  source={{ uri: tourismData ? item.timg : eventsData ? item.billPic: item.imgUrl  }} 
-                  style={styles.ImageIconStyle} 
-                />
-              </View>
-                   
-              <View style={styles.contentInfo}>
-                <Text style={[styles.nameText, eventData && { fontSize: 16}]}>{tourismData ? item.tname : eventsData ? item.billName: item.name}</Text>
-                     
-                <Text style={styles.contentText}>{tourismData ? item.taddr : item.loc}</Text>
-                { !tourismData && !eventData &&
-                  <Text style={styles.contentText}>{`Cuisine: ${item.cuisine}`}</Text>
-                }
-              </View>
-            </View>
-             <Separator />
-          </TouchableOpacity>
+    <View style={styles.container}>
+      <SafeAreaView style={{ flex: 1 }}>
+        {!tourData && !eventsData && (
+          <TextInput
+            style={styles.textInputStyle}
+            value={search}
+            underlineColorAndroid="transparent"
+            placeholder="Search Here"
+            onChangeText={(text) => searchFilter(text)}
+          />
         )}
-      />
+        <FlatList
+          data={tourismData ? tourData : eventData ? eventsData : filData}
+          keyExtractor={(item) => (tourData ? item.tname : item.name)}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              onPress={() => {
+                !tourismData && !eventData
+                  ? navigation.navigate("menu", {
+                      title: item.name,
+                      restParam: resData,
+                      foodParam: fooData,
+                      restParamId: item.uid,
+                    })
+                  : tourismData
+                  ? navigation.navigate("TourismDestination", {
+                      title: item.tname,
+                      desc: item.tdesc,
+                      img: item.timg,
+                      model: item.tmodel,
+                      url: item.turl,
+                      page: "Tourism",
+                    })
+                  : navigation.navigate("TourismDestination", {
+                      title: item.billName,
+                      desc: item.billDesc,
+                      img: item.billPic,
+                      url: item.billUrl,
+                      page: "Events",
+                    });
+              }}
+            >
+              <View style={styles.row}>
+                <View style={styles.imageHeader}>
+                  <Image
+                    source={{
+                      uri: tourismData
+                        ? item.timg
+                        : eventsData
+                        ? item.billPic
+                        : item.imgUrl,
+                    }}
+                    style={styles.ImageIconStyle}
+                  />
+                </View>
+
+                <View style={styles.contentInfo}>
+                  <Text
+                    style={[styles.nameText, eventData && { fontSize: 16 }]}
+                  >
+                    {tourismData
+                      ? item.tname
+                      : eventsData
+                      ? item.billName
+                      : item.name}
+                  </Text>
+
+                  <Text style={styles.contentText}>
+                    {tourismData ? item.taddr : item.loc}
+                  </Text>
+                  {!tourismData && !eventData && (
+                    <Text
+                      style={styles.contentText}
+                    >{`Cuisine: ${item.cuisine}`}</Text>
+                  )}
+                </View>
+              </View>
+              <Separator />
+            </TouchableOpacity>
+          )}
+        />
       </SafeAreaView>
-      </View>
-    );
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -117,58 +140,56 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginBottom: 30,
     fontSize: 28,
-    fontWeight: '500'
+    fontWeight: "500",
   },
   row: {
     paddingRight: 4,
     paddingLeft: 4,
     paddingVertical: 4,
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 10,
     marginBottom: 10,
   },
   nameText: {
-    fontWeight: '800',
-    fontSize: 20 ,
-    color: '#000',
-    flexWrap: 'wrap',
+    fontWeight: "800",
+    fontSize: 20,
+    color: "#000",
+    flexWrap: "wrap",
     flexShrink: 1,
     marginBottom: 4,
   },
   contentText: {
-    color: '#949494',
+    color: "#949494",
     fontSize: 16,
     marginTop: 2,
     marginBottom: 4,
-    flexWrap: 'wrap',
+    flexWrap: "wrap",
     flexShrink: 1,
   },
-  SeparatorLine :{
-    width: '100%',
-},
-  
-ImageIconStyle: {
-  padding: 20,
-  margin: 0,
-  height: 120,
-  width: 120,
-  resizeMode: "stretch",
-  alignContent: "flex-start",
-  marginLeft: 10,
-},
-contentInfo: {
-  alignContent: "flex-end",
-  marginLeft: 20,
-  flex : 1,
-},
-textInputStyle: {
-  height: 40,
-  borderWidth: 1,
-  paddingLeft: 20,
-  margin: 5,
-  borderColor: "#009688",
-  backgroundColor: "#FFFFFF",
-}
+  SeparatorLine: {
+    width: "100%",
+  },
+
+  ImageIconStyle: {
+    padding: 20,
+    margin: 0,
+    height: 120,
+    width: 120,
+    resizeMode: "stretch",
+    alignContent: "flex-start",
+    marginLeft: 10,
+  },
+  contentInfo: {
+    alignContent: "flex-end",
+    marginLeft: 20,
+    flex: 1,
+  },
+  textInputStyle: {
+    height: 40,
+    borderWidth: 1,
+    paddingLeft: 20,
+    margin: 5,
+    borderColor: "#009688",
+    backgroundColor: "#FFFFFF",
+  },
 });
-
-
