49488eaf3cdb2b80edff6ac9a8c9a6995b3abbe3
sai krishna <85047448+saikrishna-yarra@users.noreply.github.com>
2021-08-04 14:47:45 -0400

    CSS change - Akhilesh
    CSS change - Akhilesh

--- src/Components/TourismDescPage.js
+++ src/Components/TourismDescPage.js
@@ -12,7 +12,7 @@ import { Button } from "react-native-elements/dist/buttons/Button";
 import Separator from "./Separator";
 
 export default function TourismDescPage({ route, navigation }) {
-  const { title, desc, img, model, url } = route.params;
+  const { title, desc, img, model, url, page } = route.params;
   const [des, setDes] = useState(desc);
   const [image, setImage] = useState(img);
   const [mod, setMod] = useState(model);
@@ -20,34 +20,76 @@ export default function TourismDescPage({ route, navigation }) {
 
   return (
     <View style={styles.container}>
-         {/* <TouchableOpacity onPress={() =>  navigation.navigate("ArModels", {title: title, arModel: mod})}> */}
-         {/* <Image source={{ uri: image }} style={styles.ImageIconStyle} /> */}
-        {/* <Text> {des} </Text> */}
-        {/* <Text style={styles.bigBlue} onPress={() => Linking.openURL(website)}>Tickets</Text> */}
-        {/* </TouchableOpacity> */}
-        <Image source={{ uri: image }} style={styles.ImageIconStyle} />
-        <Separator />
-        <ScrollView style={{flex : 3, paddingHorizontal: 15, marginTop: 10, marginBottom: 15}}>
-          <Text style={styles.contentText}>
-            {des}
-          </Text>
-        </ScrollView>
+      {/* <TouchableOpacity onPress={() =>  navigation.navigate("ArModels", {title: title, arModel: mod})}> */}
+      {/* <Image source={{ uri: image }} style={styles.ImageIconStyle} /> */}
+      {/* <Text> {des} </Text> */}
+      {/* <Text style={styles.bigBlue} onPress={() => Linking.openURL(website)}>Tickets</Text> */}
+      {/* </TouchableOpacity> */}
+      <Image source={{ uri: image }} style={styles.ImageIconStyle} />
+      <Separator />
+      <ScrollView
+        style={{
+          flex: 3,
+          paddingHorizontal: 15,
+          marginTop: 10,
+          marginBottom: 15,
+        }}
+      >
+        <Text style={styles.contentText}>{des}</Text>
+      </ScrollView>
+
+      {page === "Tourism" && (
         <View style={styles.buttonContainer}>
-          <Button
-            title="View AR Model"
-            type="outline"
-            titleStyle={{fontWeight: '600'}}
-            buttonStyle={[styles.button, {marginLeft: 10, backgroundColor: '#24a0ed', borderRadius: 10}]}
-            // raised
-          />
-          <Button
-            title="Buy Tickets Online"
-            titleStyle={{fontWeight: '600'}}
-            type="outline"
-            buttonStyle={[styles.button, {marginRight: 10, backgroundColor: '#24a0ed', borderRadius: 10}]}
-            raised
-          />
+          {model &&
+            <Button
+              title="View AR Model"
+              type="outline"
+              titleStyle={{ fontWeight: '600' }}
+              buttonStyle={[styles.button, { marginLeft: 10, backgroundColor: '#24a0ed', borderRadius: 10 }]}
+              disabled={model ? false : true}
+              onPress={() => {
+                navigation.navigate("ArModels", {
+                  title: title,
+                  arModel: model,
+                })
+              }}
+            />
+          }
+          {url &&
+            <Button
+              title="Buy Tickets Online"
+              onPress={() => Linking.openURL(website)}
+              titleStyle={{ fontWeight: "600" }}
+              type="outline"
+              buttonStyle={[styles.button, { marginRight: 10, backgroundColor: '#24a0ed', borderRadius: 10 }]}
+              disabled={url ? false : true}
+              onPress={() => { url && Linking.openURL(url) }}
+            />
+          }
         </View>
+      )}
+
+      {page === "Events" && (
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity>
+            <Button
+              title="Buy Tickets Online"
+              onPress={() => Linking.openURL(website)}
+              titleStyle={{ fontWeight: "600" }}
+              type="outline"
+              buttonStyle={[
+                styles.button,
+                {
+                  // marginRight: 10,
+                  backgroundColor: "#24a0ed",
+                  borderRadius: 10,
+                },
+              ]}
+              raised
+            />
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 }
@@ -60,26 +102,26 @@ const styles = StyleSheet.create({
   },
   ImageIconStyle: {
     height: 350,
-    width: '100%',
-    resizeMode: 'stretch',
+    width: "100%",
+    resizeMode: "stretch",
   },
   contentText: {
-    color: 'grey',
+    color: "grey",
     fontSize: 20,
     marginBottom: 4,
-    flexWrap: 'wrap',
+    flexWrap: "wrap",
     flexShrink: 1,
   },
-  buttonContainer:{
-    alignSelf: 'flex-end',
+  buttonContainer: {
+    alignSelf: "flex-end",
     marginBottom: 35,
-    flexDirection: 'row',
+    flexDirection: "row",
     // justifyContent: 'flex-end',
-    alignItems: 'flex-end',
-    width:'100%',
-    justifyContent: 'space-between',
+    alignItems: "flex-end",
+    width: "100%",
+    justifyContent: "space-between",
   },
   button: {
-    width: 200
-  }
+    width: "72%",
+  },
 });
