5897556e2abc0439b2880f5974cca9fb2d8b6d5b
sai krishna <85047448+saikrishna-yarra@users.noreply.github.com>
2021-08-04 09:17:15 -0400

    Added Form
    Added Form

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import LandingPage from './src/Components/LandingPage.js';
 import ListPage from './src/Components/ListPage';
 import MenuTest from './src/Components/MenuTest.js';
@@ -17,49 +17,54 @@ export default class App extends Component {
   render() {
     return (
       <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={LandingPage}
-          options={{ title: 'Welcome', headerTitleAlign: 'center' }}
-        />
-        <Stack.Screen 
-          name="Restaurant" 
-          component={ListPage}
-          options={({ route }) => ({ title: route.params.title })} 
-        />
-        <Stack.Screen 
-          name="menu" 
-          component={MenuTest} 
-          options={({ route }) => ({ title: route.params.title })} 
-        />
-        <Stack.Screen 
-          name="Events" 
-          component={ListPage} 
-          options={({ route }) => ({ title: route.params.title })} 
-        />
-        <Stack.Screen 
-          name="Tourism" 
-          component={ListPage} 
-          options={({ route }) => ({ title: route.params.title })} 
-        />
-        <Stack.Screen 
-          name="ArModels" 
-          component={ArModelPage} 
-          options={({ route }) => ({ title: route.params.title })} 
-        />
-        <Stack.Screen 
-          name="testAr" 
-          component={TestAr} 
-          options={({ route }) => ({ title: route.params.title })} 
-        />
-        <Stack.Screen 
-          name="TourismDestination" 
-          component={TourismDescPage} 
-          options={({ route }) => ({ title: route.params.title })} 
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={LandingPage}
+            options={{ title: 'Welcome', headerTitleAlign: 'center' }}
+          />
+          <Stack.Screen
+            name="Restaurant"
+            component={ListPage}
+            options={({ route }) => ({ title: route.params.title })}
+          />
+          <Stack.Screen
+            name="menu"
+            component={MenuTest}
+            options={({ route }) => ({ title: route.params.title })}
+          />
+          <Stack.Screen
+            name="Events"
+            component={ListPage}
+            options={({ route }) => ({ title: route.params.title })}
+          />
+          <Stack.Screen
+            name="Tourism"
+            component={ListPage}
+            options={({ route }) => ({ title: route.params.title })}
+          />
+          <Stack.Screen
+            name="ArModels"
+            component={ArModelPage}
+            options={({ route }) => ({ title: route.params.title })}
+          />
+          <Stack.Screen
+            name="testAr"
+            component={TestAr}
+            options={({ route }) => ({ title: route.params.title })}
+          />
+          <Stack.Screen
+            name="TourismDestination"
+            component={TourismDescPage}
+            options={({ route }) => ({ title: route.params.title })}
+          />
+          <Stack.Screen
+            name="Client Form"
+            component={FormSubmissionPage}
+            options={({ route }) => ({ title: route.params.title })}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
