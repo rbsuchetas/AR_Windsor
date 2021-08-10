0ce7eb210d5d413ec13bbd7066a8d5428704338d
sai krishna <85047448+saikrishna-yarra@users.noreply.github.com>
2021-08-09 23:01:10 -0400

    Form validation changes - Sai Krishna
    Added email and name validation in form

--- src/Components/FormSubmissionPage.js
+++ src/Components/FormSubmissionPage.js
@@ -7,7 +7,7 @@ export default function FormSubmissionPage({ route, navigation }) {
     return (
         <WebView
             source={{
-                uri: 'https://docs.google.com/forms/d/e/1FAIpQLSfpcLEe4qlMSXTMiuQl_H-qTjQlQI4eLaJHnpqT3S_3eG7kcg/viewform'
+                uri: 'https://formfacade.com/public/110484840907423248922/all/form/1FAIpQLSfpcLEe4qlMSXTMiuQl_H-qTjQlQI4eLaJHnpqT3S_3eG7kcg'
             }}
             style={{ marginTop: 20 }}
         />
